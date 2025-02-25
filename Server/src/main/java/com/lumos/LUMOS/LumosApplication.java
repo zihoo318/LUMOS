eb980fca995dd0e86da6f5aa2b3898d34aa33656
zihoo318 <zihoo318@gmail.com>
2025-02-26 01:59:58 +0900

    start

---
+++ Server/src/main/java/com/lumos/LUMOS/LumosApplication.java
@@ -0,0 +1,13 @@
+package com.lumos.LUMOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumosApplication.class, args);
+	}
+
+}
