945c08bfbe84a58a6b8df6bf833dfe67522285f1
Yujinne <2291011@hansung.ac.kr>
2025-03-08 03:55:54 +0900

    앱 캘린더 기능 완료

--- Server/src/main/java/com/lumos/LUMOS/repository/RegisterRepository.java
+++ Server/src/main/java/com/lumos/LUMOS/repository/RegisterRepository.java
@@ -3,6 +3,17 @@ package com.lumos.LUMOS.repository;
 
 import com.lumos.LUMOS.entity.Register;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+
+import java.util.List;
 
 public interface RegisterRepository extends JpaRepository<Register, Integer> {
+    @Query("SELECT r.codeName FROM Register r " +
+            "JOIN r.user u " +
+            "WHERE r.registerDate = :date " +
+            "AND u.username = :username")
+    List<String> findCodeNamesByDate(@Param("date") LocalDate date, @Param("username") String username);
 }
