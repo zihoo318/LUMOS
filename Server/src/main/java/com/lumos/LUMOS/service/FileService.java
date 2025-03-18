4ac1d308a4124d43c0dded0405c440430854a15d
Yujinne <2291011@hansung.ac.kr>
2025-03-18 11:54:17 +0900

    미리보기 완료

--- Server/src/main/java/com/lumos/LUMOS/service/FileService.java
+++ Server/src/main/java/com/lumos/LUMOS/service/FileService.java
@@ -1,11 +1,13 @@
 package com.lumos.LUMOS.service;
 
+
 import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -22,5 +24,17 @@ public class FileService {
             throw new IOException("파일을 찾을 수 없음: " + fileName);
         }
         return new PathResource(filePath);
+    }
+
+    //텍스트 파일 내용을 문자열로 변환
+    public String loadTextFile(String fileName) throws IOException {
+        Path filePath = Paths.get(FILE_DIRECTORY).resolve(fileName).normalize();
+
+        if (!Files.exists(filePath)) {
+            throw new IOException("파일을 찾을 수 없음: " + fileName);
+        }
+
+        // 파일을 한 줄씩 읽어 문자열로 변환
+        return Files.readString(filePath, StandardCharsets.UTF_8);
     }
 }
